8f5e55f74435551cab649ca337d21ba91879ea51
mlekena_Theko <mlekena@skidmore.edu>
2016-05-10 21:54:32 -0400

    complete project

--- src/com/cradle/main/Main.java
+++ src/com/cradle/main/Main.java
@@ -65,6 +65,29 @@ public class Main {
     System.out.println(polyKnap);
     dataAnalysis(polyKnap);
     
+    ArrayList<Double> timeFor3To3In1 = new ArrayList<Double>();
+    for (int i = 0; i < 100; i++){
+      Boolean[] variables = new Boolean[]{true, false, true, false, true, false, true, false};
+      int noOfClauses = 10;
+      
+      long startTime = System.nanoTime();
+      ThreeSAT ts = new ThreeSAT(variables,noOfClauses);
+      OneInThreeSAT oitsat = OneInThreeSAT.toOneInThreeSAT(ts);
+      long endTime = System.nanoTime();
+      
+      timeFor3To3In1.add(((endTime - startTime)/1000000.0));
+      if (i == 50){
+        System.out.println("THREESAT INSTANCE SAMPLE");
+        System.out.println(ts);
+        System.out.println();
+        System.out.println("1 in 3 SAT INSTANCE FROM ABOVE 3 SAT SAMPLE");
+        System.out.println(oitsat);
+      }
+    }
+    
+    System.out.println("Reporting on 3 SAT to One IN Three SAT");
+    dataAnalysisSAT(timeFor3To3In1);
+    
    /* int length = 10;
     int budget_target = 20;
     RandomItem[] items = new RandomItem[length];
@@ -102,6 +125,20 @@ public class Main {
     System.out.println("Max = " + dataPoints.get(dataPoints.size()-1));
     System.out.println("Median = " + dataPoints.get(dataPoints.size()/2));
     int sum = 0;
+    for (int i = 0; i < dataPoints.size(); i++){
+      sum += dataPoints.get(i);
+    }
+    System.out.println("Average = " + (sum/dataPoints.size()));
+    System.out.println();
+  }
+  
+  private static void dataAnalysisSAT(ArrayList<Double> dataPoints){
+    Collections.sort(dataPoints);
+    System.out.println(dataPoints);
+    System.out.println("MIN = " + dataPoints.get(0));
+    System.out.println("Max = " + dataPoints.get(dataPoints.size()-1));
+    System.out.println("Median = " + dataPoints.get(dataPoints.size()/2));
+    double sum = 0;
     for (int i = 0; i < dataPoints.size(); i++){
       sum += dataPoints.get(i);
     }
